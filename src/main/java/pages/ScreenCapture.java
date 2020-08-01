32a0d82bcf7ffd6c5eb9a5cd71aa23b9013764d2
krychaisl1972 <krystian_golik@yahoo.co.uk>
2020-08-01 08:31:57 +0200

    ScreenShot

--- src/main/java/pages/ScreenCapture.java
+++ src/main/java/pages/ScreenCapture.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -14,13 +13,18 @@ public class ScreenCapture {
 
     public static void main(String[] args) throws IOException {
 
+        System.setProperty("webdriver.gecko.driver",
+                "src/main/resources/Drivers/geckodriver");
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.get("https://prod-kurs.coderslab.pl/index.php?controller=authentication");
+        InputPage inputPage = new InputPage(driver);
+        inputPage.loginAs("itwdnkuifyvjtqlixg@ttirv.org", "kursautomat");
 
         driver.get("https://prod-kurs.coderslab.pl/index.php?controller=history");
         driver.manage().window().maximize();
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(scrFile, new File("home/krystian/Desktop/Zadania_zaliczeniowe/src/test\\screenshot1.png"), true);
+        FileUtils.copyFile(scrFile, new File("src\\test\\Output\\screenshot1.png"), true);
         driver.quit();
     }
 }
